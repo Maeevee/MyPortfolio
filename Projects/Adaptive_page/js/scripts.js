959fbeac980962ace357fb0444e35559292fa2d5
Mariia <mariya.kuryakulova@gmail.com>
2022-07-28 02:07:54 +0300

    counter

--- Projects/Adaptive_page/js/scripts.js
+++ Projects/Adaptive_page/js/scripts.js
@@ -17,3 +17,18 @@ let header = document.querySelector("#header");
 burger.onclick = function() {
     header.classList.toggle("menu-open");
 }
+
+
+// Counter
+
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 1000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
